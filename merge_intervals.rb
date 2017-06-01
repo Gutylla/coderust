0fbf8d28b2a030f6f1ade9c0ac8fd4dfeaa05852
_AU <ashish.upadhyay@salesforce.com>
2017-06-01 00:05:47 -0700

    merge intervals, profit loss

---
+++ merge_intervals.rb
@@ -0,0 +1,26 @@
+def merge_intervals(array)
+
+  result = []
+  result.push(array.first)
+
+  array.map do |interval|
+
+    x1 = interval[0]
+    y1 = interval[1]
+    x2 = result.last[0]
+    y2 = result.last[1]
+
+    if y2 >= x1 || x2 <= y1
+      result.last[0] = [x1, x2].min
+      result.last[1] = [y1, y2].max
+    else
+      result.push(interval)
+    end
+
+  end
+
+  result
+
+end
+
+p merge_intervals([ [-111, 12], [1,5] ,[3,7] , [-3,-2] , [-5,-1], [4,6] , [-16,18] , [6,8] ,[10,12] ,[11,15], [-11, 23] ])
